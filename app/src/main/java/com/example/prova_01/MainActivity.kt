8cde24a7e3e34decc874db3e85f82a30d798c4bb
nicollas_franca <nicollas_franca@estudante.sesisenai.org.br>
2026-06-30 21:25:56 -0300

    feat: finaliza funcionalidade de registrar peça

--- app/src/main/java/com/example/prova_01/MainActivity.kt
+++ app/src/main/java/com/example/prova_01/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.prova_01
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val edtOperador = findViewById<EditText>(R.id.textOperador)
+        val edtLinhaProducao = findViewById<EditText>(R.id.textLinhaProducao)
+        val textPecas = findViewById<TextView>(R.id.txtPecas)
+
+        val btnIniciarProducao = findViewById<Button>(R.id.btnIniciarProducao)
+        val btnRegistarPeca = findViewById<Button>(R.id.btnRegistrarPeca)
+        val btnFinalizarProducao = findViewById<Button>(R.id.btnFinalizarProducao)
+
+        var contador = 0
+        var ultimoClique = 0L
+        var intervalo = 0L
+
+        btnIniciarProducao.setOnClickListener {
+
+        }
+
+        btnRegistarPeca.setOnClickListener {
+            contador++
+
+            val atual = System.currentTimeMillis()
+
+            intervalo += atual - ultimoClique
+
+            ultimoClique = atual
+
+            textPecas.text = "${"Peças: " + contador }"
+        }
+
     }
 }
